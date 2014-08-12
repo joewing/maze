3782bf70ce0e5101cdd6d0bf9fd4efbff7df3935
Joe Wingbermuehle <joewing@joewing.net>
2014-08-12 17:07:18 -0500

    Updated Haskell maze generator/solver.

--- maze.hs
+++ maze.hs
@@ -1,184 +1,147 @@
 -- Maze generator and solver in Haskell
--- by Joe Wingbermuehle
--- 20070602 <> 20070603
+-- Joe Wingbermuehle
+-- 20070602 <> 20140812
 
 module Main where
 
+import Control.Monad
+import Control.Monad.State
 import Data.Map
-import Random
+import System.Random
 
-data MazeElement
-   = Space
-   | Wall
-   | Border
-   | Solution
-   | Marked
-   deriving (Eq)
+data ElementType = Space | Wall | Marked | Visited | Door
+    deriving (Eq)
 
-instance Show MazeElement where
-   show Space = "  "
-   show Wall  = "[]"
-   show Border = show Wall
-   show Marked = "++"
+data Direction = DLeft | DRight | DUp | DDown
+    deriving (Enum)
 
-data Maze = Maze Int Int (Map Int MazeElement)
+data MazeData = MazeData {
+    width :: Int,
+    height :: Int,
+    gen :: StdGen,
+    maze :: Map (Int, Int) ElementType
+}
 
-instance Show Maze where
-   show (Maze width height fm) =
-      let lst = Data.Map.toList fm in
-      show_rows 0 width height lst
-      where
-         show_columns x width [] = ("", [])
-         show_columns x width (e:es)
-            | x < width =
-               let (str, lst) = show_columns (x + 1) width es in
-               let (key, value) = e in
-               ((show value) ++ str, lst)
-            | otherwise = ("\n", (e:es))
-         show_rows y width height lst
-            | y < height =
-               let (str, nlst) = show_columns 0 width lst in
-               str ++ (show_rows (y + 1) width height nlst)
-            | otherwise = ""
+instance Show ElementType where
+    show Space = "  "
+    show Wall = "[]"
+    show Marked = "++"
+    show Visited = show Space
+    show Door = show Space
 
--- Initialize an empty maze.
-initialize_maze :: Int -> Int -> Maze
-initialize_maze width height =
-   let lst = [ (i, Wall) | i <- [ 0 .. width * height ] ] in
-   let maze = Maze width height (Data.Map.fromList lst) in
-   let maze_b = fill_rl 0 width height $ fill_tb 0 width height maze in
-   set_element maze_b 1 1 Space
-   where
-      fill_rl y width height maze
-         | y < height =
-            let maze_l = set_element maze 0 y Border in
-            let maze_r = set_element maze_l (width - 1) y Border in
-            fill_rl (y + 1) width height maze_r
-         | otherwise = maze
-      fill_tb x width height maze
-         | x < width =
-            let maze_t = set_element maze x 0 Border in
-            let maze_b = set_element maze_t x (height - 1) Border in
-            fill_tb (x + 1) width height maze_b
-         | otherwise = maze
+instance Show MazeData where
+    show = showMaze 0 0
+        where
+            showMaze x y md =
+                if x == (width md) then
+                    if y + 1 == (height md) then ""
+                    else "\n" ++ (showMaze 0 (y + 1) md)
+                else
+                    let e = findWithDefault Wall (x, y) (maze md) in
+                    (show e) ++ (showMaze (x + 1) y md)
 
--- Set a maze element.
-set_element :: Maze -> Int -> Int -> MazeElement -> Maze
-set_element maze x y value =
-   let Maze width height fm = maze in
-   let index = x + y * width in
-   let nfm = Data.Map.insert index value (Data.Map.delete index fm) in
-   Maze width height nfm
+type MazeState a = State MazeData a
 
--- Get a maze element.
-get_element :: Maze -> Int -> Int -> MazeElement
-get_element maze x y =
-   let Maze width height fm = maze in
-   let index = x + y * width in
-   Data.Map.findWithDefault Space index fm
+-- Initialize an uncarved maze.
+initMaze :: Int -> Int -> Int -> MazeData
+initMaze w h s =
+    let xs = [0 .. w - 1] in
+    let ys = [0 .. h - 1] in
+    let top = [(x, 0) | x <- xs] in
+    let bottom = [(x, h - 1) | x <- xs] in
+    let left = [(0, y) | y <- ys] in
+    let right = [(w - 1, y) | y <- ys] in
+    let start = [(2, 2)] in
+    let spaces = Prelude.foldl (++) [] [top, bottom, left, right, start] in
+    let m1 = fromList [(p, Space) | p <- spaces] in
+    let doors = [(2, 2), (2, 1), (w - 3, h - 2)] in
+    let m2 = fromList [(p, Door) | p <- doors] in
+    MazeData {
+        width = w, height = h, gen = mkStdGen s,
+        maze = union m1 m2
+    }
 
--- Carve starting from the specified coordinates.
-do_carve_maze :: IO Maze -> Int -> Int -> IO Maze
-do_carve_maze maze x y =
-   do
-      temp <- maze
-      let value = get_element temp x y
-      dir <- getStdRandom $ randomR (0, 3)
-      if value == Space then
-         do
-            let nmaze = set_element temp x y Space
-            try_move (return nmaze) dir 0 x y
-         else maze
-   where
-      move_x :: IO Maze -> Int -> Int -> Int -> Int -> IO (Bool, Maze)
-      move_x maze x1 y1 x2 y2 =
-         do
-            temp <- maze
-            let v1 = get_element temp x1 y1
-            let v2 = get_element temp x2 y2
-            if v1 == Wall && v2 == Wall then
-               do
-                  let m1 = set_element temp x1 y1 Space
-                  let m2 = set_element m1 x2 y2 Space
-                  m3 <- do_carve_maze (return m2) x2 y2
-                  return (True, m3)
-               else
-                  do
-                     temp <- maze
-                     return (False, temp)
-      move :: IO Maze -> Int -> Int -> Int -> IO (Bool, Maze)
-      move maze 0 x y = move_x maze x (y - 1) x (y - 2)
-      move maze 1 x y = move_x maze x (y + 1) x (y + 2)
-      move maze 2 x y = move_x maze (x - 1) y (x - 2) y
-      move maze _ x y = move_x maze (x + 1) y (x + 2) y
-      try_move :: IO Maze -> Int -> Int -> Int -> Int -> IO Maze
-      try_move maze dir count x y
-         | count < 4 =
-            do
-               (status, nmaze) <- move maze dir x y
-               if status == False then
-                  do
-                     let next_dir = mod (dir + 1) 4
-                     try_move (return nmaze) next_dir (count + 1) x y
-                  else do return nmaze
-         | otherwise = maze
+-- Get the next position in the specified direction.
+getPosition :: (Int, Int) -> Direction -> (Int, Int)
+getPosition (x, y) DLeft = (x - 1, y)
+getPosition (x, y) DRight = (x + 1, y)
+getPosition (x, y) DUp = (x, y - 1)
+getPosition (x, y) DDown = (x, y + 1)
 
--- Carve starting from each possible position.
-carve_maze :: IO Maze -> IO Maze
-carve_maze maze =
-   do
-      (Maze width height _) <- maze
-      m1 <- carve_maze_y maze 1 1 width height
-      let m2 = set_element m1 0 1 Space
-      return $ set_element m2 (width - 1) (height - 2) Space
-   where
-      carve_maze_x :: IO Maze -> Int -> Int -> Int -> Int -> IO Maze
-      carve_maze_x maze x y width height
-         | x < width =
-            do
-               let nmaze = do_carve_maze maze x y
-               carve_maze_x nmaze (x + 2) y width height
-         | otherwise = maze
-      carve_maze_y :: IO Maze -> Int -> Int -> Int -> Int -> IO Maze
-      carve_maze_y maze x y width height
-         | y < height =
-            do
-               let nmaze = carve_maze_x maze x y width height
-               carve_maze_y nmaze x (y + 2) width height
-         | otherwise = maze
+-- Reverse a direction.
+invertDirection :: Direction -> Direction
+invertDirection DLeft = DRight
+invertDirection DRight = DLeft
+invertDirection DUp = DDown
+invertDirection DDown = DUp
 
--- Solve the maze (if possible).
-solve_maze :: Maze -> Maze
-solve_maze maze =
-   snd $ solve 1 1 maze
-   where
-      move maze x1 y1 x2 y2 =
-         if x2 < 0 || y2 < 0 then (False, maze)
-         else
-            let e1 = get_element maze x1 y1 in
-            let e2 = get_element maze x2 y2 in
-            if e1 == Space && e2 == Space then
-               solve x2 y2 $ set_element maze x1 y1 Marked
-            else (False, maze)
-      solve x y maze =
-         let (Maze width height _) = maze in
-         let nmaze = set_element maze x y Marked in
-         if x == width - 2 && y == height - 2 then (True, nmaze)
-         else
-            let (s_left, m_left) = move nmaze (x + 1) y (x + 2) y in
-            let (s_right, m_right) = move nmaze (x - 1) y (x - 2) y in
-            let (s_up, m_up) = move nmaze x (y - 1) x (y - 2) in
-            let (s_down, m_down) = move nmaze x (y + 1) x (y + 2) in
-            if s_left then (s_left, m_left)
-            else if s_right then (s_right, m_right)
-            else if s_up then (s_up, m_up)
-            else (s_down, m_down)
+-- Move in the specified direction.
+move :: ElementType -> (Int, Int) -> Direction -> MazeState (Int, Int)
+move fill pos d = do
+    st <- get
+    let mid = getPosition pos d
+    let m = insert mid fill $ maze st
+    let next = getPosition mid d
+    put $ st { maze = insert next fill m }
+    return next
 
--- Entry point. Generate and display the maze.
+-- Determine if we can carve in the specified direction.
+canMove :: ElementType -> (Int, Int) -> Direction -> MazeState Bool
+canMove match pos d = do
+    st <- get
+    let mid = getPosition pos d
+    let e1 = findWithDefault Wall mid $ maze st
+    let next = getPosition mid d
+    let e2 = findWithDefault Wall next $ maze st
+    return $ e1 == match && e2 == match
+
+-- Carve the maze starting at the speciified position.
+carve :: (Int, Int) -> MazeState ()
+carve pos = do
+    st <- get
+    let (start, g') = randomR (0, 3) $ gen st
+    put $ st { gen = g' }
+    mapM_ tryCarve [toEnum ((start + i) `mod` 4) | i <- [0 .. 3]]
+    where
+        tryCarve d = do
+            cm <- canMove Wall pos d
+            when cm (move Space pos d >>= carve)
+
+-- Find a path from the starting position to the end position.
+solveFrom :: (Int, Int) -> (Int, Int) -> MazeState Bool
+solveFrom pos stop
+    | pos == stop =
+        return True
+    | otherwise = do
+        st <- get
+        put $ st { maze = insert pos Marked (maze st) }
+        foldM trySolve False [toEnum i | i <- [0 .. 3]]
+    where
+        trySolve result d
+            | result = return True
+            | otherwise = do
+                cm <- canMove Space pos d
+                if cm then do
+                    found <- (move Marked pos d >>= flip solveFrom stop)
+                    if found then return True
+                    else (move Visited pos d >> return False)
+                else return False
+
+-- Generate a random maze.
+generate :: Int -> Int -> Int -> MazeData
+generate w h s = execState (carve (2, 2)) (initMaze w h s)
+
+-- Solve a maze.
+solve :: MazeData -> MazeData
+solve md =
+    let start = (2, 2) in
+    let stop = ((width md) - 3, (height md - 3)) in
+    execState (solveFrom start stop) md
+
+-- Generate and display a solved random maze.
 main :: IO ()
-main =
-   do
-      maze <- carve_maze $ return $ initialize_maze 39 23
-      putStrLn $ show maze
-      putStr $ show $ solve_maze maze
-
+main = do
+    let maze = generate 39 23 5
+    let solved = solve maze
+    putStrLn $ show maze
+    putStrLn $ show solved
